286ab782c208eef6224a33370fa809a4414ba3b2
priestine <priestine1.dev@gmail.com>
2020-06-16 17:00:32 +0300

    :fire: Remove redundant code snippet in the comment

--- src/pipes/make-changelog.ts
+++ src/pipes/make-changelog.ts
@@ -43,38 +43,3 @@ export const makeChangelog = ({ conventions }: IMakeChangelogDeps) => ({
 		${commitList.filter((commit) => conventions.bumpPatch.includes(commit.type)).map(prettifyCommit)}
 	`,
 })
-
-/*
-Const trimType = convention => change =>
-  change.replace(new RegExp(convention), "")
-
-const getMatchingChanges = (convention, changes) =>
-  changes.filter(testConvention(convention)).map(trimType(convention))
-
-const writeToChangelog = (title, items) =>
-  items.length
-    ? `\n## ${title}\n\n${items
-        .map(change => `* ${change}\n`)
-        .join("")}`
-    : ""
-
-const changelogTag = (_, version, breaks, features, fixes) =>
-  `# ${version}\n`
-    .concat(writeToChangelog("Breaking Changes", breaks))
-    .concat(writeToChangelog("Features & Deprecations", features))
-    .concat(writeToChangelog("Bug Fixes", fixes))
-
-export const MakeChangelog = ExtendPipe(
-  ({ changes, newVersion, conventions }) => ({
-    changelog: changelogTag`
-  # ${newVersion}
-  ## Breaking Changes
-  ${getMatchingChanges(conventions.major, changes)}
-  ## Features & Deprecations
-  ${getMatchingChanges(conventions.minor, changes)}
-  ## Bug Fixes
-  ${getMatchingChanges(conventions.patch, changes)}
-`,
-  }),
-)
- */
