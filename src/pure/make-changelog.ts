946dff0106b10d0d1a799dac6c7f3a2bea1157d1
priestine <priestine1.dev@gmail.com>
2020-06-24 14:25:21 +0300

    :sparkles: Add support for setting group type and description

--- src/pure/make-changelog.ts
+++ src/pure/make-changelog.ts
@@ -1,75 +1,42 @@
 import type { IRawCommit } from '../types/raw-commit'
 import type { IAppCtx } from '../types/app-ctx'
+import type { IConvention } from '../types/convention'
+import type { Version } from '../types/common-types'
 import { Either } from '../utils/either'
-import { IConvention } from '../types/convention'
 
 type Ctx = Pick<IAppCtx, 'newVersion' | 'commitList' | 'conventions'>
 
 export const makeChangelog = ({ newVersion, commitList, conventions }: Ctx) => ({
-	changelog: changelogTag`
-		# ${newVersion}
-		
-		## :boom: Breaking changes
-		${Either.fromNullable(conventions.find((convention) => convention.bumps === 'major'))
-			.chain((convention) =>
-				Either.fromNullable(
-					commitList.filter((commit) =>
-						convention.match.some((match) => new RegExp(match).test(commit.type)),
-					),
-				).map((commits) => commits.map(prettifyCommit(convention))),
-			)
-			.fold(
-				() => [],
-				(commits) => commits,
-			)}
-
-		## :sparkles: Features
-		${Either.fromNullable(conventions.find((convention) => convention.bumps === 'minor'))
-			.chain((convention) =>
-				Either.fromNullable(
-					commitList.filter((commit) =>
-						convention.match.some((match) => new RegExp(match).test(commit.type)),
-					),
-				).map((commits) => commits.map(prettifyCommit(convention))),
-			)
-			.fold(
-				() => [],
-				(commits) => commits,
-			)}
-		
-		## :bug: - :ambulance: - :lock: Fixes
-		${Either.fromNullable(conventions.find((convention) => convention.bumps === 'patch'))
-			.chain((convention) =>
-				Either.fromNullable(
-					commitList.filter((commit) =>
-						convention.match.some((match) => new RegExp(match).test(commit.type)),
-					),
-				).map((commits) => commits.map(prettifyCommit(convention))),
-			)
-			.fold(
-				() => [],
-				(commits) => commits,
-			)}
-	`,
+	changelog: `# ${newVersion}`
+		.concat(getChangelogOfType('major', { commitList, conventions }).join('\n'))
+		.concat(getChangelogOfType('minor', { commitList, conventions }).join('\n'))
+		.concat(getChangelogOfType('patch', { commitList, conventions }).join('\n'))
+		.concat(getChangelogOfType(null, { commitList, conventions }).join('\n'))
+		.concat('\n'),
 })
 
 // ------------------------------------------------------------------------------------------------
 
-const changelogTag = (
-	_: TemplateStringsArray,
-	version: string,
-	breakingChanges: string[],
-	features: string[],
-	fixes: string[],
-) =>
-	`# ${version}`
-		.concat(breakingChanges.length > 0 ? '\n\n## :boom: Breaking Changes\n\n' : '')
-		.concat(breakingChanges.join('\n'))
-		.concat(features.length > 0 ? '\n\n## :sparkles: Features\n\n' : '')
-		.concat(features.join('\n'))
-		.concat(fixes.length > 0 ? '\n\n## :bug: ∘ :ambulance: ∘ :lock: Fixes\n\n' : '')
-		.concat(fixes.join('\n'))
-		.concat('\n')
+const getChangelogOfType = (type: Version, { conventions, commitList }: Omit<Ctx, 'newVersion'>) =>
+	Either.fromNullable(conventions.find((convention) => convention.bumps === type))
+		.chain((convention) =>
+			Either.fromNullable(
+				commitList.filter((commit) =>
+					convention.match.some((match) => new RegExp(match).test(commit.type)),
+				),
+			).map((commits) =>
+				[
+					commits.length > 0 ? convention.groupTitleFormat : '',
+					commits.length > 0 ? convention.groupDescription : '',
+				]
+					.filter(Boolean)
+					.concat(commits.map(prettifyCommit(convention))),
+			),
+		)
+		.fold(
+			() => [],
+			(commits) => commits,
+		)
 
 const prettifyCommit = (convention: IConvention) => (commit: IRawCommit): string =>
 	convention.itemDescriptionFormat
