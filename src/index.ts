fbf921f20bd37d50ea0425f8ed315683d84ce87b
priestine <priestine1.dev@gmail.com>
2020-06-24 18:35:40 +0300

    :sparkles: Add --help support
    Closes #18

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,40 @@ if (argv.includes('--version')) {
 	process.exit(0)
 }
 
+if (argv.includes('--help')) {
+	const { version } = require(resolve(__dirname, '../package.json'))
+
+	logInfo`
+Priestine Versions ${version as string}
+
+A tool for automating Semantic Versioning on your project.
+
+Usage:
+
+${({ g }) => g('--config-file')}=${({ y }) =>
+		y('<path>')}			Relative custom path to a JSON, YAML or TOML config file
+${({ g }) => g('--token')}=${({ y }) => y('<token>')}				Token for publishing release	
+${({ g }) => g('--repository')}=${({ y }) => y('<owner/repo>')}		Repository URL path for publishing
+${({ g }) => g('--latest-version')}=${({ y }) =>
+		y('<version>')}		Custom version to start checking changes from
+${({ g }) => g('--prefix')}=${({ y }) => y('<string>')}			Version prefix, e.g. "v" for "v1.0.0"
+${({ g }) => g('--build-metadata')}=${({ y }) => y('<string>')}		SemVer build metadata
+${({ g }) => g('--pre-release')}=${({ y }) => y('<string>')}			SemVer Pre-Release
+${({ g }) => g('--custom-url')}=${({ y }) => y('<url>')}			Custom URL (for On-Premise users of GH/GL)
+${({ g }) => g('--merges')}=${({ y }) =>
+		y('<include|exclude|only>')}		Merge commit inclusion strategy. Default "exclude"
+${({ g }) => g('--bump-patch')}[=<true|false>]		Force bumping patch version
+${({ g }) => g('--bump-minor')}[=<true|false>]		Force bumping minor version
+${({ g }) => g('--bump-major')}[=<true|false>]		Force bumping major version
+${({ g }) => g('--public')}[=<true|false>]			Declare public API (allow bumping major versions)
+${({ g }) => g('--dry-run')}[=<true|false>]		Skip publishing new release
+
+--version				Display current @priestine/versions version
+--help					Show usage help message (this one)
+`
+	process.exit(0)
+}
+
 const readFileEither = (path: string) => Either.try<string, Error>(() => readFileSync(path, 'utf8'))
 
 const execCmdSync = execWith((cmd: string) =>
