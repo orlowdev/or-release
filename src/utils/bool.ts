3b2785893e79482e103302018de946ba0189922e
priestine <priestine1.dev@gmail.com>
2020-07-06 16:33:01 +0300

    :construction: Add (any/all).equals

--- src/utils/bool.ts
+++ src/utils/bool.ts
@@ -3,13 +3,23 @@ import type { Unary } from '../types/common-types'
 const value = Symbol('MonoidInternalValue')
 
 interface IBooleanMonoid {
+	readonly isTrue: boolean
+	readonly isFalse: boolean
+	readonly isAll: boolean
+	readonly isAny: boolean
 	[value]: boolean
+	equals: (other: IBooleanMonoid) => boolean
 	concat: (other: IBooleanMonoid) => IBooleanMonoid
 	ifTrue: (f: Unary<void, any>) => IBooleanMonoid
 	ifFalse: (f: Unary<void, any>) => IBooleanMonoid
 }
 
 export const any = <TContext = boolean>(x: TContext): IBooleanMonoid => ({
+	isTrue: Boolean(x),
+	isFalse: !x,
+	isAll: false,
+	isAny: true,
+	equals: (other) => other.isAny && Boolean(x) === other[value],
 	[value]: Boolean(x),
 	concat: (other) => any(x || other[value]),
 	ifTrue: (f) => (x ? (f(), any(x)) : any(x)),
@@ -17,6 +27,11 @@ export const any = <TContext = boolean>(x: TContext): IBooleanMonoid => ({
 })
 
 export const all = <TContext = boolean>(x: TContext): IBooleanMonoid => ({
+	isTrue: Boolean(x),
+	isFalse: !x,
+	isAll: true,
+	isAny: false,
+	equals: (other) => other.isAll && Boolean(x) === other[value],
 	[value]: Boolean(x),
 	concat: (other) => all(x && other[value]),
 	ifTrue: (f) => (x ? (f(), all(x)) : all(x)),
